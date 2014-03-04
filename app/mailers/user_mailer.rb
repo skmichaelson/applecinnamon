a3353cd2bc263329f0f5f07d93641f4c60782ae3
App Academy Student <student@appacademy.io>
2014-03-03 21:29:34 -0500

    Add sender name to mailer. More styling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def contact_me(form_data)
     @body = form_data[:body]
+    @sender = form_data[:sender_name]
     mail(from: form_data[:email], to: "sarah.michaelson@gmail.com", subject: form_data[:subject])
   end
 end
