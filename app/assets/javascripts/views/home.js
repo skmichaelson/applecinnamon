4da36b2529e6b45a1088320fbcc025bba4c834ef
App Academy Student <student@appacademy.io>
2014-03-18 14:36:23 -0400

    Move photo changing method to Backbone view

--- app/assets/javascripts/views/home.js
+++ app/assets/javascripts/views/home.js
@@ -8,5 +8,27 @@ SarahWebsite.Views.Home = Backbone.View.extend({
     var renderedContent = this.template();
     this.$el.html(renderedContent);
     return this;
+  },
+
+  events: {
+    "click .photo div": "changePhoto"
+  },
+
+  changePhoto: function (event) {
+    event.preventDefault();
+    var $pic = $(event.currentTarget);
+    var oldId = parseInt($pic.attr("data-id"));
+    var newId = (oldId + 1) % 3 + 1;
+
+    // Grab the pictures and captions with the associated ID
+    var $newPic = $('.photo').find("[data-id='" + newId + "']");
+    var $caption = $('.caption').find("[data-id='" + oldId + "']");
+    var $newCaption = $('.caption').find("[data-id='" + newId + "']");
+
+    // Swap the photos and the captions
+    $pic.removeClass("visible");
+    $caption.removeClass("visible");
+    $newPic.addClass("visible");
+    $newCaption.addClass("visible");
   }
-})
+});
