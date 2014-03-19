51f02af768af2e3689eb877ebe9995535be38274
App Academy Student <student@appacademy.io>
2014-03-19 18:42:53 -0400

    Write method to switch features on Project page

--- app/assets/javascripts/views/projects.js
+++ app/assets/javascripts/views/projects.js
@@ -1,4 +1,10 @@
 SarahWebsite.Views.Projects = Backbone.View.extend({
+
+  swapView: function(oldView, newView) {
+    oldView.removeClass("visible");
+    newView.addClass("visible");
+  },
+
   className: "group",
 
   template: JST['projects'],
@@ -7,5 +13,34 @@ SarahWebsite.Views.Projects = Backbone.View.extend({
     var renderedContent = this.template();
     this.$el.html(renderedContent);
     return this;
+  },
+
+  events: {
+    "click #next": "nextFeature",
+    "click #prev": "prevFeature"
+  },
+
+  nextFeature: function (event) {
+    event.preventDefault();
+
+    var feature = $('.visible')[0];
+    var $feature = $(feature);
+    var oldId = $feature.attr("data-id");
+    var newId = (oldId % 3) + 1;
+    var $newFeature = $('section').find("[data-id='" + newId + "']");
+
+    this.swapView($feature, $newFeature);
+  },
+
+  prevFeature: function (event) {
+    event.preventDefault();
+
+    var feature = $('.visible')[0];
+    var $feature = $(feature);
+    var oldId = $feature.attr("data-id");
+    var newId = ((oldId + 1) % 3) + 1;
+    var $newFeature = $('section').find("[data-id='" + newId + "']");
+
+    this.swapView($feature, $newFeature);
   }
 });
