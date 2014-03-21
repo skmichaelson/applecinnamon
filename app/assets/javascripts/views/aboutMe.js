4cc33f00a2d1c2880052b2afdfc456fce95eb1a4
App Academy Student <student@appacademy.io>
2014-03-21 11:10:54 -0400

    Log this.el

--- app/assets/javascripts/views/aboutMe.js
+++ app/assets/javascripts/views/aboutMe.js
@@ -11,6 +11,7 @@ SarahWebsite.Views.AboutMe = Backbone.View.extend({
   render: function() {
     console.log("In the about me render function - line 1");
     console.log(this);
+    console.log(this.el);
     var renderedContent = this.template();
     console.log("About me render function, post rendered content");
     this.$el.html(renderedContent);
