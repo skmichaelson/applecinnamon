12f1c51280eaaa0bf09445a78c84abe5e99a4b36
App Academy Student <student@appacademy.io>
2014-03-21 11:55:18 -0400

    turn off compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,7 @@ SarahWebsite::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
-  config.assets.js_compressor = Uglifier.new(mangle: false)
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
